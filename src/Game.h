3cabaaf17a28ab51d0310e9ee7ad79aec670aaf1
Aaron <sergeantbeast456@gmail.com>
2023-11-21 18:56:12 -0500

    Added the header details for Chess.h as well as dummy classes to ensure it functions

---
+++ src/Game.h
@@ -0,0 +1,10 @@
+#ifndef CPLUSPLUS_CHESS_GAME
+#define CPLUSPLUS_CHESS_GAME
+
+namespace Chess {
+    class Game {
+    // TODO - Implement
+    };
+}
+
+#endif
---
+++ src/Player.h
@@ -0,0 +1,10 @@
+#ifndef CPLUSPLUS_CHESS_PLAYER
+#define CPLUSPLUS_CHESS_PLAYER
+
+namespace Chess {
+    class Player {
+    // TODO - Implement
+    };
+}
+
+#endif
